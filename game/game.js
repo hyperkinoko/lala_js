db42342d11b074212def9fc72d32b258f5fb2822
Saki Matsumoto <kinoko@ueo.co.jp>
2017-04-01 18:07:14 +0900

    step3

--- game/game.js
+++ game/game.js
@@ -1,3 +1,29 @@
+class Enemy {
+    constructor(name, imgfile, hp_max, attack, defense, agility, exp, gold) {
+        this.name = name;
+        this.imgfile = imgfile;
+        this.hp_max = hp_max;
+        this.hp = hp_max;
+        this.attack = attack;
+        this.defense = defense;
+        this.agility = agility;
+        this.exp = exp;
+        this.gold = gold;
+    }
+
+    dispStatus() {
+        jQuery('#enemy_image img').attr('src', './images/enemy/' + this.imgfile + '.png');
+        jQuery('#enemy_status .name').text(this.name);
+        jQuery('#enemy_status .hp_max').text(this.hp_max);
+        jQuery('#enemy_status .hp').text(this.hp);
+        jQuery('#enemy_status .attack').text(this.attack);
+        jQuery('#enemy_status .defense').text(this.defense);
+        jQuery('#enemy_status .agility').text(this.agility);
+        jQuery('#enemy_status .exp').text(this.exp);
+        jQuery('#enemy_status .gold').text(this.gold);
+    }
+}
+
 function attack() {
     alert("攻撃する");
 }
@@ -19,5 +45,9 @@ function startBattle() {
     jQuery('#field_controller').hide();
     jQuery('#battle_controller').show();
     jQuery('#enemy_panel').show();
-    jQuery('#message_panel').text("敵があらわれた！");
+
+    // 敵を作る
+    var enemy = new Enemy("スリャイム", "slime", 6, 1, 2, 4, 8, 3);
+    jQuery('#message_panel').text(enemy.name + "があらわれた！");
+    enemy.dispStatus();
 }
